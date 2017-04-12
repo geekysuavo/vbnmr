652518d1da42592f54c48c0516f53ccef97afe3b
Bradley Worley <geekysuavo@gmail.com>
2017-04-12 18:24:00 +0200

    Faltering first steps to mean-field
    This commit begins the process of re-implementing mean-field
    updates for quadrature cosine factors using the VFL framework.
    
    At the moment, quad_meanfield() is providing (xi,gamma,nu)
    estimates, but evaluating xi*cos(gamma*omega+nu) does not
    yield proper results. Time for some massive back-to-basics
    debugging efforts!

--- lib/quad.c
+++ lib/quad.c
@@ -33,6 +33,13 @@ typedef struct {
    *  @vtab: quadrature modulation table.
    */
   int ***vtab;
+
+  /* mean-field related members:
+   *  @a, @x: amplitude and phase vectors.
+   *  @C, @Z: amplitude and phase matrices.
+   */
+  vector_t *a, *x;
+  matrix_t *C, *Z;
 }
 quad_t;
 
@@ -352,6 +359,57 @@ void quad_diff_cospair (const factor_t *f, const unsigned int d,
   *dtau = 0.25 * (xp * Cp + xm * Cm) / (tau * tau);
 }
 
+/* quad_cosreduce(): compute the amplitude and phase shift of a cosine
+ * formed by the linear combination of scaled and shifted cosines
+ * of equal frequency.
+ *
+ * arguments:
+ *  @a: vector of input amplitudes.
+ *  @theta: vector of input phases.
+ *  @A: pointer to the output amplitude.
+ *  @Theta: pointer to the output phase.
+ */
+void quad_cosreduce (const vector_t *a, const vector_t *theta,
+                     double *A, double *Theta) {
+  /* declare temporary scalars. */
+  double S, C, Asq;
+
+  /* get the vector lengths. */
+  const unsigned int n = a->len;
+
+  /* compute the sums required for shift computation. */
+  S = C = 0.0;
+  for (unsigned int i = 0; i < n; i++) {
+    S += vector_get(a, i) * sin(vector_get(theta, i));
+    C += vector_get(a, i) * cos(vector_get(theta, i));
+  }
+
+  /* compute the sum required for amplitude computation. */
+  Asq = 0.0;
+  for (unsigned int i = 0; i < n; i++) {
+    /* get the first pair of values. */
+    const double ai = vector_get(a, i);
+    const double ti = vector_get(theta, i);
+
+    /* include the diagonal term. */
+    Asq += ai * ai;
+
+    /* loop over the off-diagonal values. */
+    for (unsigned int j = i + 1; j < n; j++) {
+      /* get the second pair of values. */
+      const double aj = vector_get(a, j);
+      const double tj = vector_get(theta, j);
+
+      /* include the off-diagonal term. */
+      Asq += 2.0 * ai * aj * cos(ti - tj);
+    }
+  }
+
+  /* store the computed results. */
+  *A = sqrt(Asq);
+  *Theta = atan2(S, C);
+}
+
 /* --- */
 
 /* quad_mean(): evaluate the quadrature factor mean.
@@ -503,6 +561,98 @@ FACTOR_DIFF_VAR (quad) {
   }
 }
 
+/* quad_meanfield(): perform a mean-field update of a quadrature factor.
+ *  - see factor_meanfield_fn() for more information.
+ */
+FACTOR_MEANFIELD (quad) {
+  /* get the extended structure pointer. */
+  quad_t *fx = (quad_t*) f;
+
+  /* check for initialization calls. */
+  if (FACTOR_MEANFIELD_INIT) {
+    /* FIXME: handle quad mf init. */
+    return 1;
+  }
+
+  /* check for finalization calls. */
+  if (FACTOR_MEANFIELD_END) {
+    /* FIXME: handle quad mf end. */ return 0;
+    return 1;
+  }
+
+  /* gain access to the mean-field coefficients. */
+  vector_t *a = fx->a;
+  vector_t *x = fx->x;
+  matrix_t *C = fx->C;
+  matrix_t *Z = fx->Z;
+
+  /* recast the amplitude and phase matrices as vectors. */
+  vector_view_t cv = vector_view_array(C->data, f->K * f->K);
+  vector_view_t zv = vector_view_array(Z->data, f->K * f->K);
+
+  /* update along each dimension. */
+  for (unsigned int d = 0; d < f->D; d++) {
+    /* get the current dimension input. */
+    const double xd = vector_get(dat->x, d);
+
+    /* copy the amplitude coefficients. */
+    vector_copy(a, b);
+    matrix_copy(C, B);
+
+    /* compute the phase offset coefficients. */
+    for (unsigned int i = 0; i < f->K; i++) {
+      /* compute the first-order phase offsets. */
+      vector_set(x, i, M_PI_2 * VTAB(dat->p, i, d));
+
+      /* compute the second-order phase offsets. */
+      for (unsigned int j = 0; j < f->K; j++)
+        matrix_set(Z, i, j, M_PI_2 * (VTAB(dat->p, i, d) +
+                                      VTAB(dat->p, j, d)));
+    }
+
+    /* include expectations from the other dimensions. */
+    for (unsigned int delta = 0; delta < f->D; delta++) {
+      /* skip the current dimension. */
+      if (delta == d)
+        continue;
+
+      /* get the current dimension input. */
+      const double xdelta = vector_get(dat->x, delta);
+
+      /* loop to include first-order expectations. */
+      for (unsigned int i = 0; i < f->K; i++) {
+        /* compute the first-order expectation. */
+        const double phi1 = quad_cos(f, delta, xdelta,
+                                     VTAB(dat->p, i, delta));
+
+        /* update the amplitude vector. */
+        vector_set(a, i, vector_get(a, i) * phi1);
+
+        /* loop again to include second-order expectations. */
+        for (unsigned int j = 0; j < f->K; j++) {
+          /* compute the second-order expectation. */
+          const double phi2 = quad_cospair(f, delta, xdelta, xdelta,
+                                           VTAB(dat->p, i, delta),
+                                           VTAB(dat->p, j, delta));
+
+          /* update the amplitude matrix. */
+          matrix_set(C, i, j, matrix_get(C, i, j) * phi2);
+        }
+      }
+    }
+
+    /* FIXME: handle quad mf data. */
+    double xi, nu;
+    quad_cosreduce(a, x, &xi, &nu);
+    printf("%16.9le %16.9le %16.9le\n", xi, xd, nu);
+    quad_cosreduce(&cv, &zv, &xi, &nu);
+    printf("%16.9le %16.9le %16.9le\n", xi, 2.0 * xd, nu);
+  }
+
+  /* return success. */
+  return 1;
+}
+
 /* quad_div(): evaluate the quadrature factor divergence.
  *  - see factor_div_fn() for more information.
  */
@@ -537,6 +687,10 @@ FACTOR_INIT (quad) {
   quad_t *fx = (quad_t*) f;
   fx->vtab = NULL;
 
+  /* initialize the mean-field variables. */
+  fx->a = fx->x = NULL;
+  fx->C = fx->Z = NULL;
+
   /* return success. */
   return 1;
 }
@@ -548,13 +702,45 @@ FACTOR_RESIZE (quad) {
   /* get the extended structure pointer. */
   quad_t *fx = (quad_t*) f;
 
+  /* declare variables used for allocation. */
+  unsigned int nblk;
+  char *ptr = NULL;
+
+  /* determine the amount of memory required. */
+  nblk = vtab_bytes(D, K);
+  nblk += 2 * vector_bytes(K);
+  nblk += 2 * matrix_bytes(K, K);
+
   /* reallocate the phase table. */
-  fx->vtab = realloc(fx->vtab, vtab_bytes(D, K));
+  fx->vtab = realloc(fx->vtab, nblk);
   if (!fx->vtab)
     return 0;
 
   /* initialize the phase table. */
   vtab_init(fx->vtab, D, K);
+
+  /* initialize the memory address. */
+  ptr = (char*) fx->vtab;
+  ptr += vtab_bytes(D, K);
+
+  /* initialize the mean-field vector of amplitudes. */
+  fx->a = (vector_t*) ptr;
+  vector_init(fx->a, K);
+  ptr += vector_bytes(K);
+
+  /* initialize the mean-field vector of phase shifts. */
+  fx->x = (vector_t*) ptr;
+  vector_init(fx->x, K);
+  ptr += vector_bytes(K);
+
+  /* initialize the mean-field matrix of amplitudes. */
+  fx->C = (matrix_t*) ptr;
+  matrix_init(fx->C, K, K);
+  ptr += matrix_bytes(K, K);
+
+  /* initialize the mean-field matrix of phase shifts. */
+  fx->Z = (matrix_t*) ptr;
+  matrix_init(fx->Z, K, K);
 
   /* reallocate the parameter name table. */
   const unsigned int sz = P * (sizeof(char*) + 8);
@@ -562,10 +748,8 @@ FACTOR_RESIZE (quad) {
   if (!f->parnames)
     return 0;
 
-  /* create a pointer for initializing the parameter names. */
-  char *ptr = (char*) (f->parnames + P);
-
   /* initialize the parameter name addresses. */
+  ptr = (char*) (f->parnames + P);
   for (unsigned int p = 0; p < P; p++) {
     f->parnames[p] = (char*) ptr;
     ptr += 8;
@@ -654,7 +838,7 @@ static factor_type_t quad_type = {
   quad_cov,                                      /* cov       */
   quad_diff_mean,                                /* diff_mean */
   quad_diff_var,                                 /* diff_var  */
-  NULL,                                          /* meanfield */
+  quad_meanfield,                                /* meanfield */
   quad_div,                                      /* div       */
   quad_init,                                     /* init      */
   quad_resize,                                   /* resize    */
